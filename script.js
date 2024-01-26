2b660153f7fe24c00223ef483ec52698c8065c79
Ag62227 <148655432+Ag62227@users.noreply.github.com>
2024-01-25 22:27:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 document.getElementById('danceButton').addEventListener('click', () => {
-    alert("Hello! I am an alert box!!");
     const turtle = document.getElementById('coolTurtle');
     turtle.style.transform = 'rotate(90deg)';
     
 });
 
 document.getElementById('musicButton').addEventListener('click', () => {
-    alert("Hello! I am an alert box!!");
     const audio = new Audio('turtleSound.m4a'); 
     audio.play();
-    alert("Hello! I am an alert box!!");
 });
 
